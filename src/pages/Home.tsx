b417070e76a1cb0af2e15a44b6d23bff6cb978f9
Muhammad Zain <zmunawar872@gmail.com>
2026-08-03 16:33:07 +0500

    feat: remove unused useAppSelector import from Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import Hero from "../components/Hero";
 import { useAppDispatch } from "../hooks/useAppDispatch";
-import { useAppSelector } from "../hooks/useAppSelector";
 import { fetchAssets } from "../features/asset/assetThunk";
 import Features from "../components/Features";
 import AllAssets from "../components/AllAssets";
@@ -12,8 +11,6 @@ import Footer from "../components/Footer";
 
 const Home = () => {
   const dispatch = useAppDispatch();
-
-  const assetState = useAppSelector((state) => state.asset);
 
   useEffect(() => {
     dispatch(fetchAssets({ page: 1, search: "" }));
